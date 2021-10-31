303bb8e81c2c139de00558d369be161ff20d9471
amandavinci <amanthevinci@gmail.com>
2021-10-31 11:23:37 +0530

    add base class for image classification

---
+++ image_classification/datasets/base.py
@@ -0,0 +1,76 @@
+import torch
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from torch.utils.data import Dataset
+from abc import abstractmethod
+from typing import Any, List, Tuple, Dict, Optional, Callable
+
+
+class ImageClassificationDataset(Dataset):
+    """ Abstract base class for Image Classification Datasets """
+
+    @property
+    @abstractmethod
+    def url(self):
+        """ The abstract property returns the URL str to download the data from. """
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def files(self):
+        """ The abstract property returns the list of data files. """
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def classes(self):
+        """ The abstract property returns the list of classes. """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def download_data(self):
+        """ Download the data files in the data_dir. """
+        raise NotImplementedError
+
+    @abstractmethod
+    def load_data(self) -> Tuple[List[Image.Image], List[int]]:
+        """ Load the data as a list of Pillow Images and Python Integers. """
+        raise NotImplementedError
+
+    @property
+    def class_to_idx(self) -> Dict[str, int]:
+        return {class_label: i for i, class_label in enumerate(self.classes)}
+
+    def __init__(
+        self, 
+        is_train: bool, 
+        data_dir: str, 
+        transform: Callable,
+        download_data: bool = False,
+    ) -> None:
+        self.is_train = is_train
+        self.data_dir = Path(data_dir)
+        self.transform = transform
+
+        self.data_dir.mkdir(parents=True, exist_ok=True)
+        if download_data:
+            self.download_data()
+        if not self._check_exists_():
+            raise RuntimeError("Data files not found. Please download them.")
+
+        self.images, self.labels = self.load_data()
+        assert isinstance(self.images, list) and isinstance(self.labels, list)
+        assert isinstance(self.images[0], Image.Image)
+        assert isinstance(self.labels[0], int)
+
+    def __len__(self) -> int:
+        return len(self.labels)
+
+    def __getitem__(self, index: int) -> Tuple[torch.tensor, int]:
+        image, label = self.transform(self.images[index]), self.labels[index]
+        return image, label
+
+    def _check_exists_(self) -> bool:
+        return all((self.data_dir/file).exists() for file in self.files)
+    
