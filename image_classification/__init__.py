f9d78a5df572946ec900655328d623329da1df48
amandavinci <amanthevinci@gmail.com>
2021-11-02 00:39:11 +0530

    add src level imports

--- image_classification/__init__.py
+++ image_classification/__init__.py
@@ -1,3 +1,6 @@
+from image_classification.image_classification import *
+
+
 __display_name__ = "Image classification"
 __qualified_name__ = "image-classification"
 __description__ = "A deep learning project for image classification."
