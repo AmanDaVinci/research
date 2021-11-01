8dea81403579b3542b9402c0791ca7f8ef50b096
amandavinci <amanthevinci@gmail.com>
2021-11-01 20:14:17 +0530

    add training entrypoint

---
+++ image_classification/train.py
@@ -0,0 +1,20 @@
+import wandb
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
+
+from image_classification.utils import import_class
+
+
+@hydra.main(config_path="configs", config_name="config")
+def main(config: DictConfig):
+    dict_config = OmegaConf.to_container(config, resolve=True)
+    with wandb.init(project="image-classification", config=dict_config):
+        config.data_dir = f"{get_original_cwd()}/{config.data_dir}"
+        trainer_class = import_class(config.trainer.module, 
+                                     config.trainer.class_name)
+        trainer = trainer_class(config)
+        trainer.run()
+
+if __name__ == '__main__':
+    main()
