05bbca2fce13ab5d06e43e4f5e0309a87b467d43
amandavinci <amanthevinci@gmail.com>
2021-11-23 08:36:32 +0530

    add import utility

--- image_classification/utils.py
+++ image_classification/utils.py
@@ -13,11 +13,15 @@ from typing import Any, List, Tuple, Dict, Optional, Callable
 def download(from_url: str, to_file: str) -> None:
     with urlopen(from_url) as response, open(to_file, 'wb') as out_file:
         copyfileobj(response, out_file)
+
 def import_class(module: str, class_name: str) -> Any:
     return getattr(import_module(module), class_name)
 
 def import_object(module: str, object_name: str) -> Any:
     return getattr(import_module(module), object_name)
+
+def import_predictor_class(model_package: str) -> Any:
+    return getattr(import_module(model_package+".predict"), "ModelPredictor")
 
 def save(model: nn.Module, optimizer: optim, 
          loss: float, examples_seen: int, path: Path):
